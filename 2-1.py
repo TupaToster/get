3d998cbdde90cd62168bd49e68ad8f1020f38c0b
TupaToster <sidorchuk.me@phystech.edu>
2023-03-22 09:48:32 +0300

    2-1 ready kinda

--- 2-1.py
+++ 2-1.py
@@ -1,7 +1,18 @@
 import RPi.GPIO as gpio
+from time import *
 
 led = [24, 25, 8, 7, 12, 16, 20, 21]
 
 gpio.setmode (gpio.BCM)
 gpio.setup (led, gpio.OUT)
 
+for i in range (3):
+    for i in led:
+        gpio.output(i, 1)
+        sleep (0.2)
+        gpio.output(i, 0)
+        sleep (0.2)
+
+gpio.output (led, 0)
+
+gpio.cleanup()
